d5cc3c0beb52788221e7609bb6a739b9b70881a8
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-05-26 17:53:22 +0530

    nested loops

--- learn.java
+++ learn.java
@@ -4,18 +4,15 @@ public class learn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // fibonacchi series
-        System.out.print("enter a num: ");
+        // printing a pattern with nessted loops
+        System.out.println("write a num");
         int n = sc.nextInt();
-        int num1 = 0;
-        int num2 = 1;
 
-        for (int i = 0; i <= n; i++) {
-            System.out.println(num1 + " ");
-
-            int sum = num1 + num2; // new intiger to store sum of num 1,2;
-            num1 = num2;
-            num2 = sum; // shifting of the digits
+        for (int i = 1; i <= n; i++) { // outter loop for raws
+            for (int j = 1; j <= i; j++) { // inner loop for colums
+                System.out.print(" * ");
+            }
+            System.out.println(); // to move in next line
         }
     }
 }
