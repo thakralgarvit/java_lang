3c997b9ff87489d22ed2c5fa5d5a3a3e7d33a394
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-05-28 02:16:10 +0530

    reversing of array

--- learn.java
+++ learn.java
@@ -4,23 +4,19 @@ public class learn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // checking palindrom
+        // reversing the array
 
-        // input a string
-        System.out.print("input a num: ");
-        String str = sc.nextLine();
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int length = arr.length;
+        int[] orr = new int[length]; // build a dummy array to store the ans
 
-        boolean ispali = true;
-        int length = str.length();
-
-        for (int i = 1; i <= length / 2; i++) { // to make it faster
-            if (str.charAt(i) != str.charAt(length - 1 - i)) { // to check the charAt(i) not equal to end of the word
-                ispali = false;
-                break; // to get out of loop as soon as the condition is true
-            }
+        // trasfer integers from one to another array
+        for (int i = 0; i < length; i++) {
+            orr[i] = arr[length - 1 - i];
         }
-
-        System.out.println(ispali);
-
+        // printing of new array
+        for (int k = 0; k < length; k++) {
+            System.out.println(orr[k]);
+        }
     }
 }
