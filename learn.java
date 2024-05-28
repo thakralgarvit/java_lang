3ba970aeb7f74c4eac04ac4fea207ea7dd953186
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-05-28 18:20:49 +0530

    butterfly (pattern)

--- learn.java
+++ learn.java
@@ -4,21 +4,52 @@ public class learn {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // hallow sq
+        // butterfly pattern
 
-        int n = 5;
-        int m = 5;
+        int n = 4;
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                // this if is used to consider only boundaries
-                if (i == 1 || j == 1 || i == n || j == m) {
-                    System.out.print(" * ");
-                } else { // to print space
-                    System.out.print("   ");
-                }
+
+            // 1st star
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
             }
-            System.out.println(); // move to next line
+
+            // printing spaces
+            int spc = 2 * (n - i);
+
+            for (int j = 1; j <= spc; j++) {
+                System.out.print("  ");
+            }
+
+            // 2nd star
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
+
+        // 2nd half
+        for (int i = n; i >= 1; i--) {
+
+            // 1st star
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            // printing spaces
+            int spc = 2 * (n - i);
+
+            for (int j = 1; j <= spc; j++) {
+                System.out.print("  ");
+            }
+
+            // 2nd star
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
