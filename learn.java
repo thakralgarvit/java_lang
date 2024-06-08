e0d49439ee10c522b80bdf7806b4a93ac329a72e
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-06-09 05:00:20 +0530

    min, max subarray sum (array)

--- learn.java
+++ learn.java
@@ -2,30 +2,45 @@ import java.util.*;
 
 public class learn {
 
-    public static void reverse(int num[]) {
-        int first = 0;
-        int last = num.length - 1;
+    public static void subarr(int num[]) {
+        int tp = 0; // take acount of output
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int curmax = 0;
+        int currmin = 0;
 
-        while (first < last) {
-            int temp = num[last];
-            num[last] = num[first];
-            num[first] = temp;
-
-            last--;
-            first++;
+        // to get first num
+        for (int i = 0; i <= num.length - 1; i++) {
+            // to get second num
+            for (int j = i; j <= num.length - 1; j++) {
+                int sum = 0; // to get sum
+                for (int k = i; k <= j; k++) { // print and sum the integer
+                    System.out.print(num[k] + ", ");
+                    sum += num[k];
+                }
+                if (sum < min) { // checking the min condition
+                    min = sum;
+                    currmin = sum;
+                } else if (sum > max) { // checking max condition
+                    max = sum;
+                    curmax = sum;
+                }
+                System.out.print("sum is: " + sum);
+                System.out.println();
+                tp++;
+            }
+            System.out.println();
         }
+
+        System.out.println("minimum sum is: " + currmin + " & maximum is: " + curmax);
+        System.out.println("total output: " + tp);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int num[] = { 2, -1, -4, 5, 9 };
 
-        int num[] = { 2, 3, 5, 7, 9, 13 };
+        subarr(num);
 
-        reverse(num);
-
-        for (int i = 0; i < num.length; i++) {
-            System.out.print(num[i] + " ");
-        }
-        System.out.println();
     }
 }
