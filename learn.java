d0a947b85b623718f103928205f666815ce47bd6
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-06-27 12:32:30 +0530

    compression and staircase array

--- learn.java
+++ learn.java
@@ -4,11 +4,11 @@ public class learn {
 
     public static void StairCaseSearch(int arr[][], int key) {
         int row = 0;
-        int col = arr[0].length-1;
+        int col = arr[0].length - 1;
 
-        while (row < arr.length && col >= 0){
-            if (arr[row][col] == key){
-                System.out.println("key found at:" + row + ","+ col);
+        while (row < arr.length && col >= 0) {
+            if (arr[row][col] == key) {
+                System.out.println("key found at:" + row + "," + col);
                 return;
             } else if (key < arr[row][col]) {
                 col--;
@@ -19,17 +19,28 @@ public class learn {
         System.out.println("key not found");
     }
 
+    public static void compress(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if (count > 1) {
+                sb.append(count);
+            }
+        }
+        System.out.println(sb);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int arr[][] = {
-            {1,2,3,4} ,
-            {5,6,7,8,} ,
-            {9,10,11,12} ,
-            {13,14,15,16}};
-        System.out.println("enter a key");
-        int key = sc.nextInt();
-        StairCaseSearch(arr, key);
+        String str = "aaabbbchdf";
+        compress(str);
 
     }
 }
