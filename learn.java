96b229e176f4e1a03b4f2e935366373832c88ae4
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-07-23 12:48:15 +0530

    find permutation (back tracking)

--- learn.java
+++ learn.java
@@ -1,26 +1,28 @@
 import java.util.*;
 
+import javax.print.DocFlavor.STRING;
+
 public class learn {
 
     static int count = 0;
 
-    public static void find_Substring(String str, String ans, int i) {
-        //base case
-        if (i == str.length()) {
-            count++;
+    public static void find_permutation(String str, String ans){
+        // base case
+        if (str.length() == 0) {
             System.out.println(ans);
             return;
         }
-        //recursion
-        //yes choice
-        find_Substring(str, ans+str.charAt(i), i+1);
-        //no choice 
-        find_Substring(str, ans, i+1);
+        // recursion
+        for(int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String nstr = str.substring(0,i) + str.substring(i+1);
+            find_permutation(nstr, ans + curr);
+        }
     }
 
     public static void main(String[] args) {
-        String str = "abcde";
-        find_Substring(str, "", 0);
+        String str = "abc";
+        find_permutation(str, "");
         System.out.println("total no of subsets:" + (count));
         
     }
