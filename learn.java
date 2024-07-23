0ae34dd1ca56baa4a0bdc042048e4401f7a9ff1d
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-07-23 11:25:30 +0530

    Subset (Backtracking)

--- learn.java
+++ learn.java
@@ -2,35 +2,26 @@ import java.util.*;
 
 public class learn {
 
-    public static int  search(int arr[], int tar, int si, int ei) {
-        if (si > ei) {
-            return -1;
-        }
+    static int count = 0;
 
-        int mid = si + (ei - si)/2;
-
-        if (arr[mid] == tar) {
-            return mid;
+    public static void find_Substring(String str, String ans, int i) {
+        //base case
+        if (i == str.length()) {
+            count++;
+            System.out.println(ans);
+            return;
         }
-
-        if (arr[si] <= arr[mid]) { //condition for left side
-            if (arr[si] <= tar && tar <= arr[mid]) {
-                return search(arr, tar, si, mid-1);
-            } else {
-                return search(arr, tar, mid+1, ei);
-            }
-        } else { //condition for right side
-            if (tar <= arr[ei] && tar >= arr[mid]) {
-                return search(arr, tar, mid+1, ei);
-            } else {
-                return search(arr, tar, si, mid-1);
-            }
-        }
+        //recursion
+        //yes choice
+        find_Substring(str, ans+str.charAt(i), i+1);
+        //no choice 
+        find_Substring(str, ans, i+1);
     }
 
     public static void main(String[] args) {
-        int arr[] = {4,5,6,7,0,1,2,3};
-        int index = search(arr, 7, 0, arr.length-1);
-        System.out.println(index);
+        String str = "abcde";
+        find_Substring(str, "", 0);
+        System.out.println("total no of subsets:" + (count));
+        
     }
 }
