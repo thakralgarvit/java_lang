65aa4da8c04c869ae05bfd8d28176b8d50f46286
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-07-26 14:10:14 +0530

    grid ways both TC

--- learn.java
+++ learn.java
@@ -4,67 +4,40 @@ import javax.print.DocFlavor.STRING;
 
 public class learn {
 
-    static int count = 0;
-
-    public static void printboard(char board[][]) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static boolean issafe(char board[][], int row, int col) {
-        // vertical case
-        for (int i = row - 1; i >= 0; i--) {
-            if (board[i][col] == 'Q') {
-                return false;
-            }
-        }
-        // diagnal right
-        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-        // diagnal left
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void printQ(char board[][], int row) {
+    public static int gridways (int i, int j, int n, int m) {
         // base case
-        if (row == board.length) {
-            printboard(board);
-            count++;
-            System.out.println("-----chess-----"); // just to denote the end of one solution
-            return;
+        if (i == n-1 || j == m-1) {
+            return 1;
+        } else if (i == n || j == m) {
+            return 0;                                // O(2^(n+m))
         }
-        // recursion
-        for (int i = 0; i < board.length; i++) { // start with o becual we have to itarate col
-            if (issafe(board, row, i)) {
-                board[row][i] = 'Q'; // placing of queen
-                printQ(board, row + 1); // placcing of queen next row
-                board[row][i] = 'x'; // backtraking as we again go back to origianl form
-            }
+        int w1 = gridways(i+1, j, n, m);
+        int w2 = gridways(i, j+1, n, m);
+        return w1 + w2;
+    }
+
+    public static int fgridways (int n, int m) {
+        if (n == 0 || m == 0) {
+           return 1; 
         }
+        int nm1 = factorial(n-1);
+        int mm1 = factorial(m-1);
+        int up = (n-1) + (m-1);
+        int upf = factorial(up);                      // O(n+m)
+        return upf / (nm1 * mm1);
+    }
+
+    public static int factorial (int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * factorial(n-1);
     }
 
     public static void main(String[] args) {
-        int n = 5;
-        char board[][] = new char[n][n];
-        // fill the board
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                board[i][j] = 'x';
-            }
-        }
-        printQ(board, 0);
-        System.out.println("total solve are: " + count);
+        int n= 3;
+        int m = 3;
+        System.out.println(gridways(0, 0, n, m));
+        System.out.println(fgridways(n, m));
     }
 }
