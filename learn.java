33cffcfcda9a5f000b2adabff738cad24e569e34
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-08-01 16:31:31 +0530

    char combination with num

--- learn.java
+++ learn.java
@@ -4,61 +4,36 @@ import javax.print.DocFlavor.STRING;
 
 public class learn {
 
-    public static void print(int arr[][], int n) {
+    static char ch[][] = { {}, {}, { 'a', 'b', 'c' },
+            { 'd', 'e', 'f' },
+            { 'g', 'h', 'i' },
+            { 'j', 'k', 'l' },
+            { 'm', 'n', 'o' },
+            { 'p', 'q', 'r', 's' },
+            { 't', 'u', 'v' },
+            { 'w', 'x', 'y', 'z' } };
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
+    public static void combination(String str) {
+        int l = str.length();
+        if (l == 0) {
+            System.out.println("mu ba la ala");
+            return;
         }
+        bfs(0, l, new StringBuilder(), str);
     }
 
-    public static boolean isSafe(int maze[][], int x, int z, int n) {
-
-        return (x >= 0 && z >= 0 && x < n && z < n && maze[x][z] == 1);
-    }
-
-    public static boolean RatWay(int maze[][], int n) {
-        int ansMaze[][] = new int[n][n];
-
-        if (solvemaze(maze, 0, 0, ansMaze, n) == false) {
-            System.out.println("solution does not exist!!!");
-            return false;
-        }
-
-        print(ansMaze, n);
-        return true;
-    }
-
-    public static boolean solvemaze(int maze[][], int x, int z, int ansMaze[][], int n) {
-        if (x == n - 1 && z == n - 1 && maze[x][z] == 1) {
-            ansMaze[x][z] = 1;
-            return true;
-        }
-
-        if (isSafe(maze, x, z, n)) {
-            ansMaze[x][z] = 1;
-            if (solvemaze(maze, x + 1, z, ansMaze, n)) {
-                return true;
+    public static void bfs(int pos, int l, StringBuilder sb, String str) {
+        if (pos == l) {
+            System.out.println(sb.toString());
+        } else {
+            char letter[] = ch[Character.getNumericValue(str.charAt(pos))];
+            for (int i = 0; i < letter.length; i++) {
+                bfs(pos + 1, l, new StringBuilder(sb).append(letter[i]), str);
             }
-            if (solvemaze(maze, x, z + 1, ansMaze, n)) {
-                return true;
-            }
-            ansMaze[x][z] = 0;
-            return false;
         }
-        return false;
     }
 
     public static void main(String[] args) {
-        int maze[][] = { { 1, 0, 0, 0 },
-                { 1, 1, 0, 1 },
-                { 0, 1, 0, 0 },
-                { 1, 1, 1, 1 }
-        };
-        int n = maze.length;
-        RatWay(maze, n);
-
+        combination("23");
     }
 }
