b91a90c4cdd8247424cf3d159720d10d0c7021ea
GARVIT THAKRAL <thakralgarvit1@gmail.com>
2024-08-21 13:19:11 +0530

    ArrayList Start

--- learn.java
+++ learn.java
@@ -4,36 +4,17 @@ import javax.print.DocFlavor.STRING;
 
 public class learn {
 
-    static char ch[][] = { {}, {}, { 'a', 'b', 'c' },
-            { 'd', 'e', 'f' },
-            { 'g', 'h', 'i' },
-            { 'j', 'k', 'l' },
-            { 'm', 'n', 'o' },
-            { 'p', 'q', 'r', 's' },
-            { 't', 'u', 'v' },
-            { 'w', 'x', 'y', 'z' } };
-
-    public static void combination(String str) {
-        int l = str.length();
-        if (l == 0) {
-            System.out.println("mu ba la ala");
-            return;
-        }
-        bfs(0, l, new StringBuilder(), str);
-    }
-
-    public static void bfs(int pos, int l, StringBuilder sb, String str) {
-        if (pos == l) {
-            System.out.println(sb.toString());
-        } else {
-            char letter[] = ch[Character.getNumericValue(str.charAt(pos))];
-            for (int i = 0; i < letter.length; i++) {
-                bfs(pos + 1, l, new StringBuilder(sb).append(letter[i]), str);
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        combination("23");
+        ArrayList<ArrayList<Integer>> msintList = new ArrayList<>();
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(23);
+        list1.add(56);
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(23);
+        list2.add(56);
+        msintList.add(list1);
+        msintList.add(list2);
+        System.out.println(msintList);
     }
 }
